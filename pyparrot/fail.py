230c6b27905c597df7c2d4db694cef420b0bcd3d
Samuel <samsoung96@gmail.com>
2018-12-20 20:19:59 -0700

    Added the fail command

---
+++ pyparrot/fail.py
@@ -0,0 +1,15 @@
+# Fail command for bebop
+from pyparrot.Bebop import Bebop
+
+bebop = Bebop(drone_type="Bebop2")
+
+print("connecting")
+success = bebop.connect(10)
+print(success)
+
+if(success):
+	bebop.safe_land(10)
+	print("DONE - disconnecting")
+	bebop.smart_sleep(5)
+	print(bebop.sensors.battery)
+	bebop.disconnect()
