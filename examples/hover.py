7382b52c52479a7df602b7a34b34f1dcd3e41ca2
Samuel <samsoung96@gmail.com>
2019-01-25 01:34:58 -0700

    Hover file added to examples folder

--- examples/hover.py
+++ examples/hover.py
@@ -1,6 +1,6 @@
-# Author - Samuel Ambler
+# Bebop Hover example
+# - STA -
 
-# Fail command for bebop
 from pyparrot.Bebop import Bebop
 
 bebop = Bebop(drone_type="Bebop2")
@@ -9,8 +9,19 @@ print("connecting")
 success = bebop.connect(10)
 print(success)
 
-if(success):
+if (success):
+	bebop.ask_for_state_update()
+	bebop.safe_takeoff(10)
+	bebop.set_max_tilt(5)
+	bebop.set_max_vertical_speed(1)
+	bebop.set_max_altitude(2)
+
+	print("sleep 25")
+	bebop.smart_sleep(25)
+
+	print("Landing")
 	bebop.safe_land(10)
+
 	print("DONE - disconnecting")
 	bebop.smart_sleep(5)
 	print(bebop.sensors.battery)
